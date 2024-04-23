35e6aadf3e7a263f8bec89b0a90409d20de9606d
Alejandro Caballero Salas <alejandro.caballero@vivint.com>
2024-04-23 14:21:59 -0400

    first commit

---
+++ fooer_test.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestFooerTableDriven(t *testing.T) {
+	// Defining the columns of the table
+	var tests = []struct {
+		name  string
+		input int
+		want  string
+	}{
+		// the table itself
+		{"9 should be Foo", 9, "Foo"},
+		{"3 should be Foo", 3, "Foo"},
+		{"1 is not Foo", 1, "1"},
+		{"0 should be Foo", 0, "Foo"},
+	}
+	// The execution loop
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ans := Fooer(tt.input)
+			if ans != tt.want {
+				t.Errorf("got %s, want %s", ans, tt.want)
+			}
+		})
+	}
+}
+
+func TestFooerParallel(t *testing.T) {
+	fmt.Println("Alejandro Caballero Salas")
+	t.Run("Test 3 in Parallel", func(t *testing.T) {
+		t.Parallel()
+		result := Fooer(3)
+		if result != "Foo" {
+			t.Errorf("Result was incorrect, got: %s, want: %s.", result, "Foo")
+		}
+	})
+	t.Run("Test 7 in Parallel", func(t *testing.T) {
+		t.Parallel()
+		result := Fooer(7)
+		if result != "7" {
+			t.Errorf("Result was incorrect, got: %s, want: %s.", result, "7")
+		}
+	})
+}
+
+func TestFooerSkiped(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping test in short mode.")
+	}
+	result := Fooer(3)
+	if result != "Foo" {
+		t.Errorf("Result was incorrect, got: %s, want: %s.", result, "Foo")
+	}
+}
